c721bd3b6c70d3ebdc816d5ac9fa14c76d61ab78
nigel <nigelchimwene@gmail.com>
2025-02-12 16:58:49 +0300

    modified the get_meme() function to use the direct image URL instead of the preview array

--- main.py
+++ main.py
@@ -8,13 +8,15 @@ app = Flask(__name__)
 def get_meme():
     url = "https://meme-api.com/gimme"
     response = json.loads(requests.request("GET",url).text)
-    meme_large = response["preview"][-2]
+    meme_large = response["url"]
     subreddit = response["subreddit"]
     return meme_large,subreddit
 
 @app.route("/")
 def index ():
     meme_pic, subreddit = get_meme()
-    return render_template("meme_index.html", meme_pic=meme_pic, subreddit=subreddit)
-app.run(host = "0.0.0.0",port=80)
+    return render_template("index.html", meme_pic=meme_pic, subreddit=subreddit)
+
+if __name__ == "__main__": 
+   app.run(host = "0.0.0.0", port=55000, debug=True)
 
